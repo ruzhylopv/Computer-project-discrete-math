6793ceb374380a6b16f3522dbd0c4bf0655589f5
Pavlo Ruzhylo <ruzhylo.pn@ucu.edu.ua>
2024-11-15 18:55:37 +0200

    add doctests for page_rank func add name-main test

--- main.py
+++ main.py
@@ -26,19 +26,33 @@ d = {
 }
 
 len(d['A'])
-def page_rank(graph: dict) -> dict[str, int]:
+def page_rank(graph: dict) -> dict[str, list[int]]:
+    '''
+    Finds a page rank of 2 iterations of teams given as a dictionary
+    as a param graph.
+    Returns a dictionary where each key is a name of a team, and
+    each corresponding value is a list of page ranks of each iteration.
+    >>> d = {
+    ...     'A': ({'B', 'C'}, {'C'}),
+    ...     'B': ({'D'}, {'A', 'C'}),
+    ...     'D': ({'C'}, {'B', 'C'}),
+    ...     'C': ({'A', 'B', 'D'}, {'A', 'D'})
+    ... }
+    >>> page_rank(d)
+    {'A': [0.25, 0.08333333333333333, 0.125], \
+'B': [0.25, 0.20833333333333331, 0.16666666666666666], \
+'D': [0.25, 0.3333333333333333, 0.3333333333333333], \
+'C': [0.25, 0.375, 0.375]}
+    '''
     page_ranks = {key: [1/len(graph)] for key in graph}
-    # page_ranks = {list(graph.keys())[i]: [] for i in range(len(graph))}
-
-    # for key in page_ranks:
-    #     page_ranks[key] = [1/len(graph)]
     for i in range(1, 3):
         for key in graph:
-            sum_ = 0
-            for vertex in graph[key][1]:
-                sum_ += page_ranks[vertex][i-1]/len(graph[vertex][0])
+            sum_ = sum((page_ranks[vertex][i-1]/len(graph[vertex][0]) for vertex in graph[key][1]))
             page_ranks[key] += [sum_]
-
     return page_ranks
 
-print(page_rank(d))
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
+
